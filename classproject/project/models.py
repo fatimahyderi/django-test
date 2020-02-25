d9ee1779062c081bdc141d21da2fdee8325a74b0
fatimahyderi <fatimakhurram84@gmail.com>
2020-02-25 13:03:36 +0500

    admin customise

--- classproject/project/models.py
+++ classproject/project/models.py
@@ -8,13 +8,19 @@ class Achievments(models.Model):
     customers = models.IntegerField()
     photographs = models.IntegerField()
 
-
+    
 class Testimonial(models.Model):
     description = models.CharField(max_length=10380)  
     
-    
+    def __str__(self):
+        return self.description
+
 class Form(models.Model):
     Name = models.CharField(max_length=55)
     Phone_no = models.IntegerField()
     Email = models.CharField(max_length=100)
     Message = models.CharField(max_length=1000)  
+
+
+    def __str__(self):
+        return self.Name
